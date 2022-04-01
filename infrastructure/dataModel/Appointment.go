a97b53149a93d55d3c1a85dcdc0885ad59b0c466
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-01 15:07:45 +0400

    Updated domain model

--- infrastructure/dataModel/Appointment.go
+++ infrastructure/dataModel/Appointment.go
@@ -3,10 +3,11 @@ package datamodel
 import "time"
 
 type AppointmentModel struct {
-	DoctorId       string
-	PatientId      string
-	StartDateTime  time.Time
-	Duration       time.Duration
-	Summary        string
-	RequestMessage string
+	Id             string        `db:"id"`
+	DoctorId       string        `db:"doctor_id"`
+	PatientId      string        `db:"patient_id"`
+	StartDateTime  time.Time     `db:"start_date_time"`
+	Duration       time.Duration `db:"duration"`
+	Summary        string        `db:"summary"`
+	RequestMessage string        `db:"request_message"`
 }

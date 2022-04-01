b1ac2a0556837663518a9073686eb7a15f81c2de
Aranyak Ghosh <aranyakghosh@gmail.com>
2022-04-01 13:27:39 +0400

    Added placeholder Credential Repository

--- domain/repository/Credential.go
+++ domain/repository/Credential.go
@@ -1,12 +1,30 @@
 package repository
 
-import "github.com/Aranyak-Ghosh/go-sample-clinic/infrastructure"
+import (
+	"github.com/Aranyak-Ghosh/go-sample-clinic/infrastructure/datamodel"
+	"github.com/jmoiron/sqlx"
+)
 
 type credentialRepository struct {
+	db *sqlx.DB
 }
 
+var _ CredentialRespository = (*credentialRepository)(nil)
+
 type CredentialRespository interface {
-	AddUserCredentials(infrastructure.CredentialModel) error
-	FetchUserCredentialsById(string) (infrastructure.CredentialModel, error)
-	FetchUserCredentialsByEmail(string) (infrastructure.CredentialModel, error)
+	AddUserCredentials(datamodel.CredentialModel) error
+	FetchUserCredentialsById(string) (*datamodel.CredentialModel, error)
+	FetchUserCredentialsByEmail(string) (*datamodel.CredentialModel, error)
+}
+
+func (cr *credentialRepository) AddUserCredentials(model datamodel.CredentialModel) error {
+	return nil
+}
+
+func (cr *credentialRepository) FetchUserCredentialsById(id string) (*datamodel.CredentialModel, error) {
+	return nil, nil
+}
+
+func (cr *credentialRepository) FetchUserCredentialsByEmail(email string) (*datamodel.CredentialModel, error) {
+	return nil, nil
 }
